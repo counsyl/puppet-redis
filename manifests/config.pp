87ffe0a618ed367fcf174d413f75cb8678986fd6
Justin Bronn <justin@counsyl.com>
2013-10-02 13:22:17 -0700

    Use better parameter naming and set up notify/require properly with aliases.

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,15 +5,15 @@
 #
 # === Parameters
 #
-# [*config_source*]
+# [*source*]
 #  The source for the configuration file resource, default is undefined.
-#  Mutually exclusive with the `config_content` parameter.
+#  Mutually exclusive with the `content` parameter.
 #
-# [*config_content*]
+# [*content*]
 #  The content of the configuration file resource, default is undefined.
-#  Mutually exclusive with the `config_source` parameter.
+#  Mutually exclusive with the `source` parameter.
 #
-# [*config_file*]
+# [*path*]
 #  The path to the Redis configuration file, default is platform-dependent.
 #
 # [*owner*]
@@ -22,33 +22,34 @@
 # [*group*]
 #  The group of the Redis configuration file, default is 'root'.
 #
-# [*service*]
-#  The name of the redis service, default is platform-dependent.
+# [*mode*]
+#  The mode of the Redis configuration file, default is '0644'.
 #
 class redis::config(
-  $config_source  = undef,
-  $config_content = undef,
-  $config_file    = $redis::params::config_file,
-  $owner          = 'root',
-  $group          = 'root',
-  $service        = $redis::params::service,
+  $source  = undef,
+  $content = undef,
+  $path    = $redis::params::config_file,
+  $owner   = 'root',
+  $group   = 'root',
+  $mode    = '0644',
 ) inherits redis::params {
 
   # Check the configuration content and source parameters.
-  if $config_content and $config_source {
+  if $content and $source {
     fail("Cannot provide both content and source for the Redis configuration file.\n")
-  } elsif (! $config_content and ! $config_source) {
+  } elsif (! $content and ! $source) {
     fail("Must provide either the content or source for the Redis configuration file.\n")
   }
 
   # Ensure the configuration file is in place with the right content.
-  file { $config_file:
+  file { $path:
     ensure  => file,
     owner   => $owner,
     group   => $group,
-    content => $config_content,
-    source  => $config_source,
-    notify  => Service[$service],
-    require => Class['redis'],
+    mode    => $mode,
+    content => $content,
+    source  => $source,
+    notify  => Service['redis'],
+    require => Package['redis'],
   }
 }
